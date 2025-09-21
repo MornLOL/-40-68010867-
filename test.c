6660c77cd7fe53fd505afa3ef2f9b881c413614a
MornLOL <phasawut.im@gmail.com>
2025-09-21 16:08:56 +0700

    Add files via upload

--- test.c
+++ test.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>  
 #include <string.h>
 
-#define INSURANCE_FILE "insurance_data.csv"
-#define MAX_LINE_LENGTH 256
-#define MAX_FIELD_LENGTH 50
+#define Table "insurance_data.csv" //constant value 
+#define Max_Line 256
+#define Max_Field 50
 
-void show_menu();
-void show_menu(){
+void show_menu(){ //menu function
     printf("\n--- Health Insurance Management System ---\n");
     printf("1. Add a new record\n");
     printf("2. Display all records\n");
@@ -17,12 +16,54 @@ void show_menu(){
     printf("6. Exit\n");
 
     }
+void add_record(){}
+void display_records(){}
+void search_record(){}
+void update_record(){}
+void delete_record(){}
+
 
 int main(){
+    int choice;
     show_menu();
-    
-        
-    
-
-
+    printf("Enter your choice (1-6): ");
+    scanf("%d", &choice);
+    switch (choice) {
+            case 1:
+                add_record();
+                break;
+            case 2:
+                display_records();
+                break;
+            case 3:
+                search_record();
+                break;
+            case 4:
+                update_record();
+                break;
+            case 5:
+                delete_record();
+                break;
+            case 6:
+                printf("Exiting the program. Goodbye!\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please enter a number between 1 and 6.\n");
+        }
+     return 0;
+}
+void create_csv_if_not_exists() {
+    FILE *file = fopen(Table, "r");
+    if (file == NULL) {
+        file = fopen(Table, "w");
+        if (file != NULL) {
+            fprintf(file, "InsuredName,InsuranceType,StartDate,EndDate\n");
+            printf("Created a new file: %s\n", Table);
+            fclose(file);
+        } else {
+            perror("Error creating file");
+        }
+    } else {
+        fclose(file);
+    }
 }
